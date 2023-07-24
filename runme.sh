999e461c87c95225dc7957927bdd5577e269e1c8
Nitin Reddy <82951937+nitredd@users.noreply.github.com>
2023-07-24 15:43:00 +0300

    Corrected container image name

--- runme.sh
+++ runme.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker run --name approxIncrement -it -e mongouri='mongodb://host.docker.internal:27017' -e dbname='approx' -e collname='coin' -e sleeptime=2 coincount
+docker run --name docvermongodb -it -e mongouri='mongodb://host.docker.internal:27017' -e dbname='cms' -e collname='docs' -e sleeptime=2 docvermongodb
