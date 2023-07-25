2873260a5657913bd911807926ec4882e30cb557
Nitin Reddy <82951937+nitredd@users.noreply.github.com>
2023-07-25 11:34:46 +0300

    Updated the DB and Collection name to match the defaults in the app

--- runme.sh
+++ runme.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker run --name docvermongodb -it -e mongouri='mongodb://host.docker.internal:27017' -e dbname='cms' -e collname='docs' -e sleeptime=2 docvermongodb
+docker run --name docvermongodb -it -e mongouri='mongodb://host.docker.internal:27017' -e dbname='test' -e collname='contacts' -e sleeptime=2 docvermongodb
